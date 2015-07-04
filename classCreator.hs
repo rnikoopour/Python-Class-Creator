73586f55c5b7774619070e873e1b0da78b1cc601
big dhb <bigdhb@bigs-MacBook-Pro.local>
2015-07-04 13:05:08 -0700

    Curried intercalate to make code more legible

--- classCreator.hs
+++ classCreator.hs
@@ -1,22 +1,25 @@
 import System.Environment
 import Data.List
 
-
 indentDepth n = concat $ replicate (n*4) " "
 
-createFunctionHeader depth functionName parameters =  concat [(indentDepth depth), "def ", functionName,  "(", (prepareParams parameters), "):"]
+addLineBreaks = intercalate "\n"
+
+createFunctionHeader depth functionName parameters = concat [(indentDepth depth), "def ", functionName,  "(", (prepareParams parameters), "):"]
 
 createConstructorHeader members = createFunctionHeader 1 "__init__" ("self " ++ members)
 
 prepareParams = intercalate ", " . words
 
-setBasicMembers members = intercalate "\n" $ map (\paramName -> concat [indentDepth 2, "self.", paramName, " = ", paramName]) (words members)
+setBasicMember member = concat [indentDepth 2, "self.", member, " = ", member]
 
-createConstructor members = intercalate "\n" [createConstructorHeader members, setBasicMembers members]
+setBasicMembers members = addLineBreaks $ map setBasicMember (words members)
+
+createConstructor members = addLineBreaks $ [createConstructorHeader members, setBasicMembers members]
 
 createClassHeader className = concat ["class ", className, ":"]
 
-createClass className members = intercalate "\n" [createClassHeader className, createConstructor members]
+createClass className members = addLineBreaks $ [createClassHeader className, createConstructor members]
 
 main = do
   args <- getArgs
